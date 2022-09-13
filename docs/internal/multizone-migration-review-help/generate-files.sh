b9006762b391eccb5cbe7575aa5e22679900c739
György Krajcsovits <gyorgy.krajcsovits@grafana.com>
2022-09-13 08:19:45 +0200

    Replace replica count template with 3 in steps
    Signed-off-by: György Krajcsovits <gyorgy.krajcsovits@grafana.com>

--- docs/internal/multizone-migration-review-help/generate-files.sh
+++ docs/internal/multizone-migration-review-help/generate-files.sh
@@ -11,12 +11,14 @@ for component in alertmanager ingester storegateway ; do
   find step0 -type f -print0 | xargs -0 "${SED}" -E -i -- "/^\s+(checksum\/config):/d"
   i=1
   while [ -e "${component}-step${i}.yaml" ] ; do
+    step_yaml="${component}-step${i}.yaml"
     step_dir="step${i}"
     prev=`expr $i - 1`
     prev_dir="step$(expr $i - 1)"
     echo "Component=${component} Prev step=${prev_dir} Current step=${step_dir}"
     rm -rf "${step_dir}"
-    helm template krajo ../../../../operations/helm/charts/mimir-distributed --output-dir "${step_dir}" -f ../base.yaml -f "${component}-step${i}.yaml"
+    ${SED} -i "s/<N>/3/g" "${step_yaml}"
+    helm template krajo ../../../../operations/helm/charts/mimir-distributed --output-dir "${step_dir}" -f ../base.yaml -f "${step_yaml}"
     find "${step_dir}" -type f -print0 | xargs -0 "${SED}" -E -i -- "/^\s+(checksum\/config):/d"
     diff -c -r "${prev_dir}" "${step_dir}" > "diff-${prev_dir}-${step_dir}.patch"
     ((i++))
