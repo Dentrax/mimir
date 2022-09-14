37ad4ef8c32f91ee0485701a0a0846666ed0559c
György Krajcsovits <gyorgy.krajcsovits@grafana.com>
2022-09-14 09:59:17 +0200

    Get rid of versions in the saved templates, pollutes the diffs
    Signed-off-by: György Krajcsovits <gyorgy.krajcsovits@grafana.com>

--- docs/internal/multizone-migration-review-help/generate-files.sh
+++ docs/internal/multizone-migration-review-help/generate-files.sh
@@ -8,7 +8,7 @@ for component in alertmanager ingester storegateway ; do
   pushd "${component}"
   rm -rf step0
   helm template krajo ../../../../operations/helm/charts/mimir-distributed --output-dir step0 -f ../base.yaml
-  find step0 -type f -print0 | xargs -0 "${SED}" -E -i -- "/^\s+(checksum\/config):/d"
+  find step0 -type f -print0 | xargs -0 "${SED}" -E -i -- "/^\s+(checksum\/config|(helm.sh\/)?chart|app.kubernetes.io\/version|image: \"grafana\/(mimir|mimir-continuous-test|enterprise-metrics)):/d"
   i=1
   while [ -e "${component}-step${i}.yaml" ] ; do
     step_yaml="${component}-step${i}.yaml"
@@ -19,7 +19,7 @@ for component in alertmanager ingester storegateway ; do
     rm -rf "${step_dir}"
     ${SED} -i "s/<N>/3/g" "${step_yaml}"
     helm template krajo ../../../../operations/helm/charts/mimir-distributed --output-dir "${step_dir}" -f ../base.yaml -f "${step_yaml}"
-    find "${step_dir}" -type f -print0 | xargs -0 "${SED}" -E -i -- "/^\s+(checksum\/config):/d"
+    find "${step_dir}" -type f -print0 | xargs -0 "${SED}" -E -i -- "/^\s+(checksum\/config|(helm.sh\/)?chart|app.kubernetes.io\/version|image: \"grafana\/(mimir|mimir-continuous-test|enterprise-metrics)):/d"
     diff -c -r "${prev_dir}" "${step_dir}" > "diff-${prev_dir}-${step_dir}.patch"
     ((i++))
   done
